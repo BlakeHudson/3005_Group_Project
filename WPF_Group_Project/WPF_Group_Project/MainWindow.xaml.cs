c935e2ad36d6e775ebdf26d483c2d5d7d6adaf2a
simonstockton763 <61173796+simonstockton763@users.noreply.github.com>
2020-11-27 21:24:27 -0600

    Revert "Blake logic"

--- WPF_Group_Project/WPF_Group_Project/MainWindow.xaml.cs
+++ WPF_Group_Project/WPF_Group_Project/MainWindow.xaml.cs
@@ -22,40 +22,7 @@ namespace WPF_Group_Project
     {
         public MainWindow()
         {
-            
             InitializeComponent();
-            
-        }
-
-        private void SelectBtn_Click(object sender, RoutedEventArgs e)
-        {
-            //this will open the truck rental window for the selected listbox member
-        }
-
-        private void EditBtn_Click(object sender, RoutedEventArgs e)
-        {
-            AddPrompt addPrompt = new AddPrompt();
-            addPrompt.Show();
-            //this function will open the add window with the variables from the selected listbox member
-            //It will probably have to delete the old member and just make a copy of it
-        }
-
-        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
-        {
-            CustomerList.Items.RemoveAt(CustomerList.SelectedIndex);
-            //this function removes the highlighted customer
-        }
-
-        private void AddBtn_Click(object sender, RoutedEventArgs e)
-        {
-            CustomerList.Items.Add("New Customer template");
-            //This function opens a new window to add a new customer to the listbox source
-        }
-
-        private void Searchbtn_Click(object sender, RoutedEventArgs e)
-        {
-            //This function will update listbox with a new list based on search query
-            Title = Search.Text;
         }
     }
 }
