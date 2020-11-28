a44fb37636c63e8702937f0a1a11eef181257e9d
BlakeHudson <blakehudson615@gmail.com>
2020-11-27 20:29:39 -0600

    Customer and customer list methods updates

--- WPF_Group_Project/WPF_Group_Project/CustomerList.cs
+++ WPF_Group_Project/WPF_Group_Project/CustomerList.cs
@@ -14,8 +14,39 @@ namespace WPF_Group_Project
         {
             this.customers = new List<Customer>();
         }
+        // Adds customer to end of list
+        public void AddCustomer(Customer c)
+        {
+            customers.Add(c);
+        }
+        // Removes a customer at index provided
+        public void RemoveCustomerAtIndex(int i)
+        {
+            customers.RemoveAt(i);
+        }
 
-        public string show()
+        /*
+         * Searches for a customer by lastname, firstname. 
+         * returns index of a matched customer
+         * else returns -1 if no matching name in list
+         */
+        public int GetCustomerIndex(string ln, string fn)
+        {
+            int i = -1;
+            foreach(Customer c in customers)
+            {
+                if(c.LastName == ln && c.FirstName == fn)
+                {
+                    return customers.IndexOf(c);
+                }
+            }
+            return i;
+        }
+
+        /*
+         * Returns one larger string of all customers in list
+         */
+        public override string ToString()
         {
             string cl = "";
             foreach(Customer c in customers)
