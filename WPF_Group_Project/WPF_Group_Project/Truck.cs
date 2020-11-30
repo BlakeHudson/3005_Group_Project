cfc1799681c4b948c14912eec5d0a943c4c1e476
Marcus Stange <marcus.g.stange@gmail.com>
2020-11-29 22:45:33 -0600

    Added and modified functions

--- WPF_Group_Project/WPF_Group_Project/Truck.cs
+++ WPF_Group_Project/WPF_Group_Project/Truck.cs
@@ -24,19 +24,29 @@ namespace WPF_Group_Project
         // Returns true of truck is rented, false if available to be rented
         public bool IsRented(Truck t)
         {
-            if(t.Status == true)
+            /*if(t.Status == true) This is unnessary complex since Status is a bool already
             {
                 return true;
             }
             else
             {
                 return false;
-            }
+            }*/
+            return t.Status;
         }
+        public bool IsRented()
+        {
+            return Status;
+        }
+
         //Sets Status to false when truck is returned from being rented
         public void ReturnTruck(Truck t)
         {
             t.Status = false;
+        }
+        public void ReturnTruck()
+        {
+            Status = false;
         }
         //Sets Status to true when truck is rented
         public void RentedTruck(Truck t)
@@ -44,19 +54,25 @@ namespace WPF_Group_Project
             t.Status = true;
         }
 
+        public void RentedTruck()
+        {
+            Status = true;
+        }
+
         public void SetSize(string s)
         {
-            if(s == "small")
+            s = s.ToLower();
+            if(s == "small" || s == "s" || s == "1")
             {
-                this.Size = 1;
+                Size = 1;
             }
-            if (s == "medium")
+            if (s == "medium" || s=="m" || s == "2")
             {
-                this.Size = 2;
+                Size = 2;
             }
-            if (s == "large")
+            if (s == "large" || s == "l" || s == "3")
             {
-                this.Size = 3;
+                Size = 3;
             }
         }
 
@@ -71,6 +87,26 @@ namespace WPF_Group_Project
                 return "medium";
             }
             if (s == 3)
+            {
+                return "large";
+            }
+            else
+            {
+                return "unknown";
+            }
+        }
+
+        public string GetSize()
+        {
+            if (Size == 1)
+            {
+                return "small";
+            }
+            if (Size == 2)
+            {
+                return "medium";
+            }
+            if (Size == 3)
             {
                 return "large";
             }
